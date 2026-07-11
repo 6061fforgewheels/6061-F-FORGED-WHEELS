d445f184db9b1eae80f0d6eb3a94a98e1cec6a45
6061fforgewheels <erstwheels@gmail.com>
2026-07-11 22:24:53 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/6061-f-forged-wheels/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
